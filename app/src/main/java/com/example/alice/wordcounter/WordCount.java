4de13f058fc68d5a88b06b53678b9bf0905bdb3c
Alice Rees <alice.rees90@gmail.com>
2017-09-18 21:53:32 +0100

    word count homework

---
+++ app/src/main/java/com/example/alice/wordcounter/WordCount.java
@@ -0,0 +1,47 @@
+package com.example.alice.wordcounter;
+
+import android.text.Editable;
+import android.widget.EditText;
+
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import static com.example.alice.wordcounter.R.id.text;
+
+/**
+ * Created by alice on 18/09/2017.
+ */
+
+public abstract class WordCount {
+    public HashMap<String, Integer> wordsByCount;
+//    public Scanner counter;
+
+    public WordCount (EditText textToCount) {
+    }
+
+    public void count(String text) {
+        String[] textToCount = text.split(" ");
+        Map<String, Integer> wordsByCount = new HashMap<>();
+        for (String word: textToCount) {
+            Integer count = wordsByCount.get(word);
+            count = (count == null) ? 1 : ++count;
+            wordsByCount.put(word, count);
+        }
+    }
+
+//    public void count (String text) {
+//        Scanner count = new Scanner(text);
+//        Map<String, Integer> counts = new HashMap<>();
+//        while(count.hasNext()) {
+//            String word = count.next();
+//            if( !counts.containsKey(word) ){
+//            counts.put(word, 1);
+//            }
+//            else
+//                counts.put(word, counts.get(word) +1 );
+//        }
+//    }
+
+}
---
+++ app/src/main/java/com/example/alice/wordcounter/WordCountActivity.java
@@ -0,0 +1,32 @@
+package com.example.alice.wordcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class WordCountActivity extends AppCompatActivity {
+    EditText textToCount;
+    TextView countedText;
+    Button countButton;
+    WordCount wordCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_count);
+
+        textToCount = (EditText) findViewById(R.id.textToCount);
+        countedText = (TextView) findViewById(R.id.countedText);
+        countButton = (Button) findViewById(R.id.countButton);
+    }
+
+    public void onCountButtonClicked(View button) {
+        String text = textToCount.getText();
+        String textCounted = wordCount.count(text);
+        countedText.setText(count);
+    }
+}
